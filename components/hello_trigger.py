5c5e06f5b48872d4140f8a59e07529a83f1c3dfa
ly_ecust <ly_ecust@outlook.com>
2022-09-19 11:43:34 +0800

    delete some files

--- components/hello_trigger.py
+++ components/hello_trigger.py
@@ -1,25 +1,15 @@
+import time
 import suanpan
 from suanpan.app import app
-from suanpan.app.arguments import Int, Float, Bool
-from suanpan.app.loops import IntervalIndex
-
-DEFAULT_INTERVAL = 5
-loop = IntervalIndex(DEFAULT_INTERVAL)
+from suanpan.app.arguments import String, Json
 
 
-@app.trigger.afterInit
-def initTrigger(context):
+@app.trigger.interval(2)
+@app.param(String(key="prefix", alias="prefix"))
+@app.trigger.output(Json(key="outputData1"))
+def hello_trigger(context):
     args = context.args
-    loop.set(seconds=10, pre=args.pre, disabled=args.disabled)
-
-
-@app.trigger.loop(loop)
-@app.trigger.output(Int(key="outputData1", alias="index"))
-@app.trigger.param(Float(key="param1", alias="seconds", default=DEFAULT_INTERVAL))
-@app.trigger.param(Bool(key="param2", alias="pre", default=False))
-@app.trigger.param(Bool(key="param3", alias="disabled", default=False))
-def SPTrigger(context, n):
-    return n
+    return f'hello trigger, {args.prefix} @ {time.time()}'
 
 
 if __name__ == "__main__":
