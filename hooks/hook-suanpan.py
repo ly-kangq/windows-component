e6a8047962cb69165c6098e2321b86e46e266872
ly_ecust <ly_ecust@outlook.com>
2022-11-07 14:24:01 +0800

    repair pyinstaller problem

--- hooks/hook-suanpan.py
+++ hooks/hook-suanpan.py
@@ -3,6 +3,7 @@ from PyInstaller.utils.hooks import collect_all
 from PyInstaller.utils.hooks import logger
 
 os.environ['SP_APP_TYPE'] = 'stream'
+os.environ['SP_APP_ID'] = 'test_app_id'
 os.environ['SP_NODE_ID'] = 'test_node_id'
 datas, binaries, hiddenimports = collect_all('suanpan', include_py_files=False)
 
